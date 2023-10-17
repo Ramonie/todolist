a91cbfc3638f228981b7a0431e606b71a03a2123
Ramonie <ramonmartinsmr@gmail.com>
2023-10-17 20:26:31 -0300

    Update de tarefas: validando as permissões de usuários

--- src/main/java/martins/ramonie/todolist/task/TaskController.java
+++ src/main/java/martins/ramonie/todolist/task/TaskController.java
@@ -46,13 +46,24 @@ public class TaskController {
     }
     //http://localhost:8080/tasks/id
     @PutMapping("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id){
-        var idUser = request.getAttribute("idUser");
+    public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id){
+
 
         var task = this.taskRepository.findById(id).orElse(null);
-        Utils.copyNonNullProperties(taskModel, task);
+        if ( task == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).
+                    body("Tarefa não encontrada");
+        }
+        var idUser = request.getAttribute("idUser");
+        if (!task.getIduser().equals(idUser)) {
+          return  ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                  .body("Usuário não tem permissão para alterar essa tarefa");
+        }
 
-        return this.taskRepository.save(task);
+        Utils.copyNonNullProperties(taskModel, task);
+        var  taskUpdated = this.taskRepository.save(task);
+        return ResponseEntity.ok().body(this.taskRepository.save(taskUpdated));
+
     }
 
 }
